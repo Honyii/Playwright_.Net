69e55645489a31ca263847e47015dab8c015d9ed
amaechi hope <amaechihope20@gmail.com>
2024-08-30 17:01:56 +0100

    add login test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -11,11 +11,12 @@ public class OrangeHrm : PageTest
     public async Task HasTitle()
     {
         await Page.GotoAsync("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-
-      
         await Expect(Page).ToHaveTitleAsync(new Regex("OrangeHRM"));
+        await Page.Locator("input[name='username']").FillAsync("Admin");
+        await Page.Locator("input[name='password']").FillAsync("admin123");
+        await Page.Locator("button[type='submit']").ClickAsync();
+        await Expect(Page).ToHaveURLAsync(new Regex(".*dashboard"));
     }
 
-   
 }
 
