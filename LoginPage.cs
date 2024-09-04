8bad913e272e2653fa0d11ab6996845c4f84027b
amaechi hope <amaechihope20@gmail.com>
2024-09-04 20:41:51 +0100

    update tests

---
+++ LoginPage.cs
@@ -0,0 +1,37 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace PlaywrightTests.Pages
+{
+    public class LoginPage
+    {
+        private readonly IPage _page;
+        public LoginPage(IPage page)
+        {
+            _page = page;
+        }
+
+        public async Task NavigateToLoginPage()
+        {
+            await _page.GotoAsync("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        }
+
+        public async Task PerformLogin(string username, string password)
+        {
+            await _page.Locator("input[name='username']").FillAsync(username);
+            await _page.Locator("input[name='password']").FillAsync(password);
+            await _page.Locator("button[type='submit']").ClickAsync();
+        }
+
+        public async Task VerifyPageTitle()
+        {
+            await Assertions.Expect(_page).ToHaveTitleAsync(new Regex("OrangeHRM"));
+        }
+
+        public async Task VerifyDashboardUrl()
+        {
+            await Assertions.Expect(_page).ToHaveURLAsync(new Regex(".*dashboard"));
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,22 +1,24 @@
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using NUnit.Framework;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
-using NUnit.Framework;
+using PlaywrightTests.Pages;
 
-namespace PlaywrightTests;
-public class OrangeHrm : PageTest
+namespace PlaywrightTests
 {
-    [Test]
-    public async Task LoginTest()
-    {
-        await Page.GotoAsync("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        await Expect(Page).ToHaveTitleAsync(new Regex("OrangeHRM"));
-        await Page.Locator("input[name='username']").FillAsync("Admin");
-        await Page.Locator("input[name='password']").FillAsync("admin123");
-        await Page.Locator("button[type='submit']").ClickAsync();
-        await Expect(Page).ToHaveURLAsync(new Regex(".*dashboard"));
-    }
 
+    [TestFixture]
+    public class OrangeHrm : PageTest
+    {
+        [Test]
+        public async Task LoginTest()
+        {
+            var loginPage = new LoginPage(Page);
+            await loginPage.NavigateToLoginPage();
+            await loginPage.VerifyPageTitle();
+            await loginPage.PerformLogin("Admin", "admin123");
+            await loginPage.VerifyDashboardUrl();
+            await Page.PauseAsync();
+        }
+    }
 }
 
