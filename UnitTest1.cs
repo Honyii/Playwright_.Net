de95ffb4f0332f47df19a4bb1e64ca540e8c9092
amaechi hope <amaechihope20@gmail.com>
2024-09-15 12:40:27 +0100

    add json data

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -2,6 +2,8 @@ using NUnit.Framework;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using My.Testing;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace PlaywrightTests
 {
@@ -9,13 +11,18 @@ namespace PlaywrightTests
     [TestFixture]
     public class OrangeHrm : PageTest
     {
+        private class LoginData {
+            public string Username {get; set;}
+            public string Password {get; set;}
+        }
         [Test]
         public async Task LoginTest()
         {
+            var loginData = JsonConvert.DeserializeObject<LoginData>(File.ReadAllText("data.json"));
             var loginPage = new LoginPage(Page);
             await loginPage.NavigateToLoginPage();
             await loginPage.VerifyPageTitle();
-            await loginPage.PerformLogin("Admin", "admin123");
+            await loginPage.PerformLogin(loginData.Username, loginData.Password);
             await loginPage.VerifyDashboardUrl();
             await Page.PauseAsync();
         }
